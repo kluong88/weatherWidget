df23872ad6a37806a437322998fa329b8152af8f
Kevin Luong <luongk88@gmail.com>
2020-05-19 17:29:35 -0500

    Current conditions now display on webpage based on fetched data

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const apiKey = `0fc73a1ffced9111b9e2a4e94bb3eb58`;
+const currentConditions = document.querySelector(`.current-conditions`);
+
 
 
 navigator.geolocation.getCurrentPosition(position => {
   console.log(position.coords.latitude, position.coords.longitude);
+  getCurrentConditions(position.coords.latitude, position.coords.longitude);
+});
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
+function getCurrentConditions(latitude, longitude) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`)
     .then(resp => {
       if (resp.ok) {
         return resp.json();
@@ -12,11 +17,13 @@ navigator.geolocation.getCurrentPosition(position => {
         throw new Error("Invalid search input. Please try again.");
       }
     })
-    .then(json => {
-      console.log(json);
-      console.log(json.main.feels_like);
-      console.log(json.weather[0].description);
-      console.log(json.weather[0].icon);
-
+    .then(currentConditionsResults => {
+      currentConditions.innerHTML = `
+      <h2>Current Conditions</h2>
+      <img src="http://openweathermap.org/img/wn/${currentConditionsResults.weather[0].icon}@2x.png" />
+      <div class="current">
+        <div class="temp">${Math.ceil(currentConditionsResults.main.feels_like)}℃</div>
+        <div class="condition">${currentConditionsResults.weather[0].description}</div>
+      `
     });
-});
+}
