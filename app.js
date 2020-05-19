b18677b000526e20a15b3c9cf5f1ef6e78465005
Kevin Luong <luongk88@gmail.com>
2020-05-19 18:12:23 -0500

    Split the 5 day forecast array into smaller arrays of 8. Each array of 8 represents a single daily forecast.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const apiKey = `0fc73a1ffced9111b9e2a4e94bb3eb58`;
-
+const currentConditions = document.querySelector(`.current-conditions`);
 
 navigator.geolocation.getCurrentPosition(position => {
-  console.log(position.coords.latitude, position.coords.longitude);
+  getCurrentConditions(position.coords.latitude, position.coords.longitude);
+  getForecast(position.coords.latitude, position.coords.longitude);
+});
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
+function getCurrentConditions(latitude, longitude) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`)
     .then(resp => {
       if (resp.ok) {
         return resp.json();
@@ -12,11 +15,49 @@ navigator.geolocation.getCurrentPosition(position => {
         throw new Error("Invalid search input. Please try again.");
       }
     })
-    .then(json => {
-      console.log(json);
-      console.log(json.main.feels_like);
-      console.log(json.weather[0].description);
-      console.log(json.weather[0].icon);
-
+    .then(currentConditionsResults => {
+      currentConditions.innerHTML = `
+      <h2>Current Conditions</h2>
+      <img src="http://openweathermap.org/img/wn/${currentConditionsResults.weather[0].icon}@2x.png" />
+      <div class="current">
+        <div class="temp">${Math.ceil(currentConditionsResults.main.temp)}℃</div>
+        <div class="condition">${currentConditionsResults.weather[0].description}</div>
+      `
     });
-});
+}
+
+function getForecast(latitude, longitude) {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`)
+    .then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Invalid search input. Please try again.");
+      }
+    })
+    .then(forecastResults => {
+      console.log(forecastResults.list);
+      getDailyForecast(forecastResults.list, 8);
+
+      forecastResults.list.forEach(results => {})
+    })
+}
+
+function getDailyForecast(array, size) {
+  const dailyForecastArr = [];
+  for (let x = 0; x < array.length; x++) {
+    const last = dailyForecastArr[dailyForecastArr.length - 1];
+    if (!last || last.length === size) {
+      dailyForecastArr.push([array[x]]);
+    } else {
+      last.push(array[x]);
+    }
+  }
+  console.log(dailyForecastArr);
+  dailyForecastArr.forEach(dailyCondition => {
+    console.log(dailyCondition[3].weather[0].description);
+  })
+
+
+  console.log(dailyForecastArr[0][3].weather[0].description);
+}
