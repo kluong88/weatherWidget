6a64998d57ab4b6af870e6de0178825422e72391
Kevin Luong <luongk88@gmail.com>
2020-05-20 22:10:45 -0500

    Completed data object sorted by weekday code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const apiKey = `0fc73a1ffced9111b9e2a4e94bb3eb58`;
 const currentConditionsEle = document.querySelector(`.current-conditions`);
 const forecastEle = document.querySelectorAll(`.day`);
+let date = new Date();
+let currentDay = date.getDay();
 
 navigator.geolocation.getCurrentPosition(position => {
   getCurrentConditions(position.coords.latitude, position.coords.longitude);
@@ -38,96 +40,74 @@ function fetchForecast(latitude, longitude) {
     })
     .then(forecastResults => {
       filterForecastData(forecastResults.list);
-    })
-}
-
-function filterForecastData(forecastResults) {
-
-  forecastResults.forEach(timeStamp => {
-    timeStamp.dayOfWeek = new Date(timeStamp.dt_txt).toLocaleString('en-us', { weekday: 'long' });
-  });
-
-  sortForecastData(forecastResults);
+    });
 };
 
-function sortForecastData(filteredForecastData) {
-  currentDay = new Date().toLocaleString('en-us', { weekday: 'long' });
-
+function filterForecastData(forecastResults) {
   let filteredForecastArr = [];
   let dateArray = [];
-  let day = [];
 
+  forecastResults.forEach(timeStamp => {
+    timeStampDate = new Date(timeStamp.dt_txt);
+    timeStamp.weekday = new Date(timeStamp.dt_txt).toLocaleString('en-us', { weekday: 'long' });
+    timeStamp.weekCode = timeStampDate.getDay();
+  });
 
-  filteredForecastArr = filteredForecastData.filter(timeStamp => timeStamp.dayOfWeek != currentDay);
+  filteredForecastArr = forecastResults.filter(timeStamp => timeStamp.weekCode != currentDay);
 
-  filteredForecastArr.forEach(timeStamp => {
-    console.log(timeStamp);
-    day = [{ day: timeStamp.dayOfWeek }]
-  })
+  dateArray = filteredForecastArr.reduce(function(week, forecast) {
+    if (week[forecast.weekCode] === undefined) {
+      week[forecast.weekCode] = [];
+      week[forecast.weekCode].push({
+        // dt_txt: forecast.dt_txt,
+        weekday: forecast.weekday,
+        weekCode: forecast.weekCode,
+        max_temp: forecast.main.temp_max,
+        min_temp: forecast.main.temp_min,
+        description: forecast.weather[0].description,
+        icon: forecast.weather[0].icon,
+      })
+    } else
+      week[forecast.weekCode].push({
+        // dt_txt: forecast.dt_txt,
+        weekday: forecast.weekday,
+        weekCode: forecast.weekCode,
+        max_temp: forecast.main.temp_max,
+        min_temp: forecast.main.temp_min,
+        description: forecast.weather[0].description,
+        icon: forecast.weather[0].icon,
+      })
 
-  console.log(day);
+    return week;
+  }, {
 
+  });
 
+  updateForecast(dateArray);
+};
 
+function updateForecast(dailyForecast) {
 
-}
+  console.log(dailyForecast[0]);
 
+  let nextForecast = currentDay + 1;
 
-
-
-// const weekdays = {
-//   0: [],
-//   1: [],
-//   2: [],
-//   3: [],
-//   4: [],
-//   5: [],
-//   6: [],
-// }
-
-// filteredForecastData.forEach(timeStamp => {
-//   if (timeStamp.dayOfWeek != currentDay) {
-//     filteredForecastArr.push(timeStamp);
-//   }
-// })
-
-// // console.log(filteredForecastArr);
-
-// filteredForecastArr.forEach(timeStamp => {
-//   if (timeStamp.dayOfWeek == `Monday`) {
-//     weekdays[0].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Tuesday`) {
-//     weekdays[1].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Wednesday`) {
-//     weekdays[2].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Thursday`) {
-//     weekdays[3].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Friday`) {
-//     weekdays[4].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Saturday`) {
-//     weekdays[5].push(timeStamp);
-//   } else if (timeStamp.dayOfWeek == `Sunday`) {
-//     weekdays[6].push(timeStamp);
-//   }
-// })
-// updateForecast(weekdays);
-
-
-
-
-function updateForecast(dailyData) {
-
-  console.log(dailyData);
-  let insertCount = 0;
-  forecastEle[insertCount].innerHTML = `
-          <h3>Tuesday</h3>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" />
-          <div class="description">clear sky</div>
+  for (let x = 0; x < dailyForecast[x].length; x++) {
+    dailyForecast[x].forEach(data => {
+      console.log(data.weekday);
+      console.log(data.max_temp);
+    })
+  }
+  for (let x = 0; x < forecastEle.length; x++) {
+    forecastEle[x].innerHTML = `
+          <h3>${dailyForecast[nextForecast][0].weekday}</h3>
+          <img src="http://openweathermap.org/img/wn/${dailyForecast[0][0].icon}@2x.png" />
+          <div class="description">${dailyForecast[0][0].description}</div>
           <div class="temp">
             <span class="high">11℃</span>/<span class="low">-3℃</span>
           </div>
 `
-
+  }
 }
 
 // let temp_maxArr = [];
